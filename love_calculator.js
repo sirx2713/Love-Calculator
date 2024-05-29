09eda4c6f39451c195e7f973b206670f7730c082
Mabende <belindamabende@gmail.com>
2024-05-29 15:03:18 +0200

    I love calculating!

---
+++ love_calculator.js
@@ -0,0 +1,4 @@
+var firstName = prompt("Enter First Name");
+var lastName = prompt("Enter Last Name");
+var n = Math.random();
+alert("Hi " + firstName + " " + lastName + " your love is at " + Math.floor(n * 100) + "%");
